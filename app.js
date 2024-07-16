7af58304651f4eeef66106c63de1175a9cbcf068
Meiddiel <tokymickaelrivo@gmail.com>
2024-07-16 22:21:27 +0300

    very starting point for our boss challenge

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/blogBD", {useNewUrlParser:true});
+mongoose.connect("mongodb://localhost:27017/blogDB");
 
-const postSchema = new mongoose.Schema({
-  title : String,
-  content : String
-});
+const postSchema = {
+  title: String,
+  content: String
+};
 
 const Post = mongoose.model("Post", postSchema);
 
@@ -28,40 +28,46 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 let posts = [];
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.render('home', {
     startingContent: homeStartingContent,
     postsContent: posts
   });
 });
 
-app.get("/about", function(req, res) {
+app.get("/about", function (req, res) {
   res.render('about', { aboutContent: aboutContent });
 });
 
-app.get("/contact", function(req, res) {
+app.get("/contact", function (req, res) {
   res.render('contact', { contactContent: contactContent });
 });
 
-app.get("/compose", function(req, res) {
+app.get("/compose", function (req, res) {
   res.render('compose');
 });
 
-app.post("/compose", function(req, res) {
-  const post = new Post({
+app.post("/compose", function (req, res) {
+  const composition = new Post({
     title: req.body.postTitle,
     content: req.body.postContent
   });
 
-  post.save();
+  composition.save()
+    .then(function () {
+      console.log("Successfuly saved")
+    })
+    .catch(function () {
+      console.log("Error")
+    });
 
   res.redirect("/")
 
 });
 
-app.get('/posts/:postName', function(req, res) {
+app.get('/posts/:postName', function (req, res) {
   const requestedTitle = _.lowerCase(req.params.postName);
-  posts.forEach(function(post) {
+  posts.forEach(function (post) {
     const storedTitle = _.lowerCase(post.title);
     if (requestedTitle === storedTitle) {
       console.log('Match found!');
@@ -83,6 +89,6 @@ app.get('/posts/:postName', function(req, res) {
 
 
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log("Server started on port " + port);
 });
