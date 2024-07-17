690576f62e0c306e553acba247c4284e65440d07
meiddiel <tokymickaelrivo@gmail.com>
2024-07-17 16:42:23 +0300

    render in our home page all recorded post in our DB

--- app.js
+++ app.js
@@ -26,12 +26,15 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "I'm RARIVOARISOA Toky Mickael and what you are seeing now is my First blog page. Dont expect anything from this web site, It's only purpose is for the understanding of web development. Actually, I'm a web developer student. I dont have many project and just wanna improve my programing skills. The roadmap will be long and sometimes difficulte and borring but I assume that I will enjoy my journey. I hope, i will become full flage web developer and work on a big project that will change the world";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-let posts = [];
-
 app.get("/", function (req, res) {
-  res.render('home', {
-    startingContent: homeStartingContent,
-    postsContent: posts
+  Post.find({})
+    .then (function(posts){
+    res.render('home', {
+      startingContent: homeStartingContent,
+      postsContent: posts
+    });
+  }).catch(function(error){
+    console.log(error);
   });
 });
 
